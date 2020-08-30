dd90101f65fd41ff6ce9eb5018f13e2e2ebcc3c3
Javiergm18 <63000031+Javiergm18@users.noreply.github.com>
2020-08-30 10:52:36 -0500

    Add files via upload

---
+++ taller/Home.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace taller
+{
+    /// <summary>
+    /// Lógica de interacción para Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = txtProductName.Text;
+
+            txtResults.Text = productName;
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = (MainWindow)Window.GetWindow(this);
+            w.frameMain.NavigationService.Navigate(new Login());
+        }
+    }
+}
